89a5d44ef3d846d26e8d5297a2d8ea60596fd080
LeoMucci <leocapramucci@gmail.com>
2023-10-06 16:10:16 -0300

    06/10

--- Home.tsx
+++ Home.tsx
@@ -1,18 +1,64 @@
-import { TextComponent, View, Text } from "react-native";
+
+import React from "react";
+import { TextComponent, View, Text, FlatList, StyleSheet } from "react-native";
+import { Card} from "react-native-paper";
+
+const dados = [
+    {
+        'id':1,
+        'titulo':'A volta dos que não foram',
+        'autor':'John Nobody',
+        'editora':'Fechou'
+    },
+    {
+        'id':2,
+        'titulo':'Fogo na caixa dagua',
+        'autor':'Joe Nobody',
+        'editora':'Fechou'
+    },
+
+]
 
 
 export default function Home(){
     return(
+        <FlatList 
+        data={dados}
+        renderItem={({item}) => 
+            <Card style={estilo.cardEstilo}>
+                
+                <Card.Title title={item.titulo}/>
+                <Card.Cover resizeMode="center"source={{uri:'https://ayine.com.br/wp-content/uploads/2022/03/Miolo-diagonal-O-livro-dos-amigos-site.png'}}/>
+                    <Card.Content>
 
-        <View>
-            <View>
+                        <Text>{item.autor}</Text>
+                        <Text>{item.editora}</Text>
 
-                <Text> A Volta dos que não foram</Text>
+                    </Card.Content>
+                  
+            </Card>
+                
 
-            </View>
 
-        </View>
+            
+        }
+        keyExtractor={(item)=>item.id.toString()}
+        >
+
+
+        </FlatList>
 
 
     );
 }
+
+const estilo = StyleSheet.create(
+{
+        cardEstilo: {
+            margin:5,
+            display:"flex",
+            
+        }
+}
+
+);
