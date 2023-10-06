1a0c8fdbb38ca79bfc25c5414d71551718c369f2
LeoMucci <leocapramucci@gmail.com>
2023-10-06 16:08:23 -0300

    aula 06/10

--- App.tsx
+++ App.tsx
@@ -1,11 +1,16 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import Home from './Home';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
       <StatusBar style="auto" />
+
+      <Home></Home>
+
+
+
     </View>
   );
 }
@@ -13,8 +18,9 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: '#b20000',
+    alignItems: 'flex-start',
+    verticalAlign: 'top',
+    justifyContent: 'flex-start',
   },
 });
---
+++ Home.tsx
@@ -0,0 +1,18 @@
+import { TextComponent, View, Text } from "react-native";
+
+
+export default function Home(){
+    return(
+
+        <View>
+            <View>
+
+                <Text> A Volta dos que não foram</Text>
+
+            </View>
+
+        </View>
+
+
+    );
+}
